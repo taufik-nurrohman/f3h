c3f9fff4e1865da41d9119362a3f2216a0eb2360
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-04-14 20:32:08 +0700

    Test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# f3h
-Tiny AJAX pages library, for modern browsers.
+F3H: Fetch
+==========
+
+> Tiny AJAX system that converts conventional HTML page navigation system into AJAX-based HTML page navigation system, for modern browsers.
--- f3h.js
+++ f3h.js
@@ -1,6 +1,19 @@
+/*!
+ * ==============================================================
+ *  F3H (FETCH) 0.0.0
+ * ==============================================================
+ * Author: Taufik Nurrohman <https://github.com/taufik-nurrohman>
+ * License: MIT
+ * --------------------------------------------------------------
+ */
+
 (function(win, doc, name) {
 
     var instances = 'instances';
+
+    function attributeGet(node, attr) {
+        return node.getAttribute(attr);
+    }
 
     function doPreventDefault(e) {
         e.preventDefault();
@@ -26,12 +39,16 @@
         return 'string' === typeof x;
     }
 
-    function sourcesGet(query, source) {
-        return (source || doc).querySelectorAll(query);
+    function refGet() {
+        return win.location.href;
     }
 
-    function valueGet(node, prop) {
-        return prop in node ? node[prop] : node.getAttribute(prop);
+    function toCaseLower(x) {
+        return x.toLowerCase();
+    }
+
+    function toCaseUpper(x) {
+        return x.toUpperCase();
     }
 
     (function($$) {
@@ -44,13 +61,21 @@
 
         var $ = this,
             $$ = win[name],
-            firstHref = win.location.href,
             hooks = {},
+            home = '//' + win.location.hostname,
+            ref = refGet(),
             requests = {},
             sources = {},
             state = {
-                'sources': 'a',
-                'fetch': 'href',
+                'sources': 'a[href],form[action]',
+                'is': function(element) {
+                    var target = element.target,
+                        to = attributeGet(element, 'href') || attributeGet(element, 'action');
+                    if (target && target !== '_self') {
+                        return false;
+                    }
+                    return "" === to || -1 !== ['.', '/', '?'].indexOf(to[0]) || 0 === to.search(home) || 0 === to.search(win.location.protocol + home);
+                },
                 'lot': {
                     'X-Requested-With': name
                 }
@@ -64,57 +89,73 @@
         // Store current instance to `F3H.instances`
         $$[instances][Object.keys($$[instances]).length] = $;
 
-        if (isFunction(o) || isString(o)) {
-            state.fetch = o; // Do nothing!
+        function sourcesGet(query, root) {
+            return Array.from((root || doc).querySelectorAll(query)).filter(state.is || function() {
+                return false;
+            });
         }
 
         state = Object.assign(state, o);
         sources = sourcesGet(state.sources);
 
-        function doAbortAllFetch() {
-            if (!requests.length) {
-                return;
-            }
-            for (var request in requests) {
-                requests[request][0].abort();
-                hookFire('abort', [doc, requests[request][1]]);
-                delete requests[request];
-            }
-        }
-
-        function doFetch(el, method, to) {
-            hookFire('exit', [doc, el]);
-            var type = state.type,
-                lot = state.lot,
+        function doFetch(node, type, ref) {
+            hookFire('exit', [doc, node]);
+            var headers = state.lot,
+                header, data,
+                parts = ref.split('#'),
                 xhr = new XMLHttpRequest;
-            xhr.responseType = type || 'document';
-            xhr.open(method.toUpperCase(), to);
-            if (lot && lot.length) {
-                for (var k in lot) {
-                    xhr.setRequestHeader(k, lot[k]);
+            xhr.responseType = state.type || 'document';
+            xhr.open(type, ref);
+            if (headers && headers.length) {
+                for (header in headers) {
+                    xhr.setRequestHeader(header, headers[header]);
                 }
             }
             xhr.onload = function() {
-                var data = [xhr.response, el];
+                data = [xhr.response, node];
+                hookFire($.status = xhr.status, data);
                 hookFire('success', data);
-                hookFire(xhr.status, data);
                 sources = sourcesGet(state.sources);
                 onSourcesEventsSet();
+                if (parts[1]) {
+                    var target = doc.getElementById(parts[1]);
+                    if (target) {
+                        doc.documentElement.scrollLeft = doc.body.scrollLeft = target.offsetLeft;
+                        doc.documentElement.scrollTop = doc.body.scrollTop = target.offsetTop;
+                    }
+                }
             };
             xhr.onerror = function() {
-                var data = [xhr.response, el];
+                data = [xhr.response, node];
                 hookFire('error', data);
                 sources = sourcesGet(state.sources);
                 onSourcesEventsSet();
             }
-            xhr.send(); // TODO: Post request(s)
+            xhr.send('POST' === type ? new FormData(node) : null);
             return xhr;
         }
 
-        function doLocationChange(el, href) {
+        function doFetchAbort(id) {
+            if (requests[id]) {
+                requests[id][0].abort();
+                hookFire('abort', [doc, requests[id][1]]);
+                delete requests[id];
+            }
+        }
+
+        function doFetchAbortAll() {
+            if (!requests.length) {
+                return;
+            }
+            for (var request in requests) {
+                doFetchAbort(request);
+            }
+        }
+
+        function doRefChange(el, ref) {
             win.history.pushState({
-                href: href
-            }, doc.title, href);
+                ref: ref
+            }, doc.title, ref);
         }
 
         function hookLet(name, fn) {
@@ -155,52 +196,53 @@
             return $;
         }
 
-        function onClick(e) {
-            doAbortAllFetch();
+        function onFetch(e) {
+            doFetchAbortAll();
             var t = this,
-                to = state.fetch,
-                href = valueGet(t, isFunction(to) ? to.call($, t) : to), method;
-            requests[href] = [doFetch(t, method = (valueGet(t, 'method') || 'get').toLowerCase(), href), t];
-            if ('get' === method) {
-                doLocationChange(t, href);
+                href = t.href,
+                action = t.action,
+                ref = href || action,
+                type = toCaseUpper(t.method || 'GET');
+            if ('GET' === type && 'popstate' !== e.type) {
+                doRefChange(t, href);
             }
+            requests[ref] = [doFetch(t, type, ref), t];
             doPreventDefault(e);
         }
 
         function onPopState(e) {
-            doAbortAllFetch();
-            var href = e.state ? e.state.href : firstHref;
+            doFetchAbortAll();
+            var href = e.state ? e.state.ref : ref;
             if (href) {
-                requests[href] = [doFetch(win, 'get', href), win];
+                requests[href] = [doFetch(win, 'GET', href), win];
             }
-            console.log([e,this])
         }
 
         function onSourcesEventsSet() {
             for (var i = 0, j = sources.length; i < j; ++i) {
-                eventSet(sources[i], 'click', onClick);
+                eventSet(sources[i], 'FORM' === toCaseUpper(sources[i].nodeName) ? 'submit' : 'click', onFetch);
             }
         }
 
-        $.abort = function(href) {
-            if (!href) {
-                doAbortAllFetch();
-            } else if (requests[href]) {
-                requests[href][0].abort();
-                hookFire('abort', [doc, requests[href][1]]);
+        $.abort = function(id) {
+            if (!id) {
+                doFetchAbortAll();
+            } else if (requests[id]) {
+                doFetchAbort(id);
             }
             return $;
         };
 
         $.pop = function() {
-            eventLet(win, 'popstate', onPopState);
-        }
+            return eventLet(win, 'popstate', onPopState), hookFire('pop', [doc, win]), $;
+        };
 
         $.fire = hookFire;
         $.hooks = hooks;
         $.off = hookLet;
         $.on = hookSet;
         $.sources = sources;
+        $.status = null;
 
         eventSet(win, 'DOMContentLoaded', onSourcesEventsSet);
         eventSet(win, 'popstate', onPopState);
