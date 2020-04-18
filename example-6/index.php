dd295cda239c672c64ffe107eab52ed3ce9939e3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-04-18 10:03:18 +0700

    Add File Upload Example

---
+++ example-6/index.php
@@ -0,0 +1,59 @@
+<?php
+
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    if (!empty($_FILES['blob'])) {
+        if (empty($_FILES['blob']['error'])) {
+            move_uploaded_file($_FILES['blob']['tmp_name'], __DIR__ . '/' . uniqid() . '.' . pathinfo($_FILES['blob']['name'], PATHINFO_EXTENSION));
+            header('Location: ?error=0');
+            exit;
+        }
+        header('Location: ?error=' . $_FILES['blob']['error']);
+        exit;
+    }
+    header('Location: ?error');
+    exit;
+}
+
+?><!DOCTYPE html>
+<html dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width" name="viewport">
+    <title>Home</title>
+  </head>
+  <body>
+    <main>
+      <h1>Files</h1>
+      <form action="" enctype="multipart/form-data" method="post">
+        <?php if ($files = glob(__DIR__ . '/*.*')): ?>
+        <ol>
+          <?php foreach ($files as $file): ?>
+          <?php if ('index.php' === ($n = basename($file))) continue; ?>
+          <li><a href="<?= $n; ?>" target="_blank"><?= $n; ?></a></li>
+          <?php endforeach; ?>
+          </ol>
+        <?php endif; ?>
+        <p>
+          <input name="blob" type="file">
+          <button type="submit">Upload</button>
+        </p>
+      </form>
+    </main>
+    <script src="../f3h.min.js"></script>
+    <script>
+
+    var f3h = new F3H,
+        main = document.querySelector('main');
+
+    f3h.on('exit', function(response, any) {
+        document.title = (any.blob ? 'Uploading' : 'Loading') + '…';
+    });
+
+    f3h.on(200, function(response) {
+        document.title = response.title;
+        main.innerHTML = response.querySelector('main').innerHTML;
+    });
+
+    </script>
+  </body>
+</html>
